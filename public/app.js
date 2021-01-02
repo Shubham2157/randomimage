2848d4f61fe4b05cac40c3da18c648b0e079f2a0
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2021-01-02 21:10:42 +0530

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,22 @@
+var nextBtn = document.querySelector("#nextbtn")
+var img = document.querySelector(".image")
+var head = document.querySelector("p")
+
+var isbrown = false;
+
+setInterval(function(){
+    if(isbrown){
+        head.style.background = "aqua"
+        head.style.color = "brown";
+    }
+    else{
+        head.style.background = "brown"
+        head.style.color = "aqua";
+    }
+    isbrown = !isbrown
+}, 1000)
+
+nextBtn.addEventListener("click", function(){
+    location.reload();
+    // img.setAttribute("src","https://source.unsplash.com/random")
+})
